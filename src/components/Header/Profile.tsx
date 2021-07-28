64d27c90af8571e019217ddbd786eb95684d2d0d
Arthur bernardo <arthurutschgg@gmail.com>
2021-07-28 08:39:43 -0300

    Header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,18 @@
 import { Flex, Text, Box, Avatar } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
     return (
         <Flex align="center">
-            <Box mr="4" textAlign="right">
-                <Text>Arthur Bernardo</Text>
-                <Text color="gray.300" fontSize="small">Contato.arthurbernardoas@gmail.com</Text>
-            </Box>
+            {showProfileData && (
+                <Box mr="4" textAlign="right">
+                    <Text>Arthur Bernardo</Text>
+                    <Text color="gray.300" fontSize="small">Contato.arthurbernardoas@gmail.com</Text>
+                </Box>
+            )}
             <Avatar size="md" name="Arthur Bernardo" src="https://github.com/ArthurBernardoDev.png" />
         </Flex>
     );
