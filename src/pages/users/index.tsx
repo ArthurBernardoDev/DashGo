d75bc8e28976f29c99cc7f8435d7f6df2318f9b8
Arthur bernardo <arthurutschgg@gmail.com>
2021-08-13 16:26:38 -0300

    listagem de usuarios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -13,7 +13,20 @@ export default function UserList() {
         const response = await fetch('http://localhost:3000/api/users')
         const data = await response.json()
 
-        return data;
+        const users = data.users.map(user => {
+            return {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+                    day: '2-digit',
+                    month: 'long',
+                    year: 'numeric'
+                })
+            }
+        });
+
+        return users;
     })
 
 
@@ -75,54 +88,26 @@ export default function UserList() {
                                 </Thead>
 
                                 <Tbody>
-                                    <Tr>
-                                        <Td px={["4", "4", "6"]}>
-                                            <Checkbox colorScheme="pink" />
-                                        </Td>
-                                        <Td>
-                                            <Box>
-                                                <Text fw="bold" >Arthur Bernardo</Text>
-                                                <Text fontSize="sm" color="gray.300">contato.arthurbernardoas@gmail.com</Text>
-                                            </Box>
-                                        </Td>
-                                        {isWideVersion &&
-                                            <Td>
-                                                03 de Abril, 2021
-                                            </Td>
-                                        }
-                                    </Tr>
-                                    <Tr>
-                                        <Td px={["4", "4", "6"]}>
-                                            <Checkbox colorScheme="pink" />
-                                        </Td>
-                                        <Td>
-                                            <Box>
-                                                <Text fw="bold" >Arthur Bernardo</Text>
-                                                <Text fontSize="sm" color="gray.300">contato.arthurbernardoas@gmail.com</Text>
-                                            </Box>
-                                        </Td>
-                                        {isWideVersion &&
-                                            <Td>
-                                                03 de Abril, 2021
-                                            </Td>
-                                        }
-                                    </Tr>
-                                    <Tr>
-                                        <Td px={["4", "4", "6"]}>
-                                            <Checkbox colorScheme="pink" />
-                                        </Td>
-                                        <Td>
-                                            <Box>
-                                                <Text fw="bold" >Arthur Bernardo</Text>
-                                                <Text fontSize="sm" color="gray.300">contato.arthurbernardoas@gmail.com</Text>
-                                            </Box>
-                                        </Td>
-                                        {isWideVersion &&
-                                            <Td>
-                                                03 de Abril, 2021
-                                            </Td>
-                                        }
-                                    </Tr>
+                                    {data.map(user => {
+                                        return (
+                                            <Tr key={user.id}>
+                                                <Td px={["4", "4", "6"]}>
+                                                    <Checkbox colorScheme="pink" />
+                                                </Td>
+                                                <Td>
+                                                    <Box>
+                                                        <Text fw="bold" >{user.name}</Text>
+                                                        <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                                                    </Box>
+                                                </Td>
+                                                {isWideVersion &&
+                                                    <Td>
+                                                        {user.createdAt}
+                                                    </Td>
+                                                }
+                                            </Tr>
+                                        )
+                                    })}
                                 </Tbody>
                             </Table>
                             <Pagination />
